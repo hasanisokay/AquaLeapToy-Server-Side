eb569ff7ffbe9f76a889beb7c18bf23e84a9759f
hasanisokay <hasanisokay@gmail.com>
2023-05-19 15:16:34 +0600

    delete signle toy added

--- index.js
+++ index.js
@@ -37,6 +37,12 @@ async function run() {
         
     })
     
+    app.delete("/myToys/:id",async(req,res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await toyCollection.deleteOne(query)
+      res.send(result)
+    })
     
     
     
